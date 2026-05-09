abba574cdbd26ed522f737b69de6023eeed10d2c
momoko <32352488+1imoonlight@users.noreply.github.com>
2026-05-10 06:09:53 +0800

    fix

--- embykeeper/telegram/link.py
+++ embykeeper/telegram/link.py
@@ -238,19 +238,19 @@ class Link:
                 )
                 authed_services.setdefault(self.client.me.id, {})[service] = bool(result)
                 return bool(result)
-            else:
-                try:
-                    await self.post(
-                        f"/auth {service} {self.instance}",
-                        name=f"服务 {service.upper()} 认证",
-                        fail=True,
-                    )
-                except LinkError as e:
-                    log_func(f"初始化错误: 使用 {service.upper()} 服务, 但{e}")
-                    if "权限不足" in str(e):
-                        await self._show_super_ad()
-                    authed_services.setdefault(self.client.me.id, {})[service] = False
-                    return False
+            # else:
+            #     try:
+            #         await self.post(
+            #             f"/auth {service} {self.instance}",
+            #             name=f"服务 {service.upper()} 认证",
+            #             fail=True,
+            #         )
+            #     except LinkError as e:
+            #         log_func(f"初始化错误: 使用 {service.upper()} 服务, 但{e}")
+            #         if "权限不足" in str(e):
+            #             await self._show_super_ad()
+            #         authed_services.setdefault(self.client.me.id, {})[service] = False
+            #         return False
                 else:
                     authed_services.setdefault(self.client.me.id, {})[service] = True
                     return True
